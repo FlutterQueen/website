7f0a41de40b6879addc185f77679d9eb6d7203d5
Ahmed Masoud <maxzod66@gmail.com>
2021-12-04 01:01:17 +0200

    update features

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,105 +6,111 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: "Flutter Queen",
-    tagline: "Queen Support for Flutter framework",
-    url: "https://flutterqueen.github.io",
-    baseUrl: "/website/",
-    onBrokenLinks: "throw",
-    onBrokenMarkdownLinks: "warn",
-    favicon: "img/favicon.ico",
-    organizationName: "FlutterQueen", // Usually your GitHub org/user name.
-    projectName: "website", // Usually your repo name.
+  title: "Flutter Queen",
+  tagline: "Queen Support for Flutter framework",
+  url: "https://flutterqueen.github.io",
+  baseUrl: "/website/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "FlutterQueen", // Usually your GitHub org/user name.
+  projectName: "website", // Usually your repo name.
 
-    presets: [
-        [
-            "@docusaurus/preset-classic",
-            /** @type {import('@docusaurus/preset-classic').Options} */
-            ({
-                docs: {
-                    sidebarPath: require.resolve("./sidebars.js"),
-                    // Please change this to your repo.
-                    editUrl: "https://github.com/facebook/docusaurus/edit/main/website/",
-                },
-                blog: {
-                    showReadingTime: true,
-                    // Please change this to your repo.
-                    editUrl: "https://github.com/facebook/docusaurus/edit/main/website/blog/",
-                },
-                theme: {
-                    customCss: require.resolve("./src/css/custom.css"),
-                },
-            }),
-        ],
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl: "https://github.com/facebook/docusaurus/edit/main/website/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/main/website/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
     ],
+  ],
 
-    themeConfig:
+  themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-        ({
-        navbar: {
-            title: "Queen 👑",
-            logo: {
-                alt: "My Site Logo",
-                src: "img/logo.svg",
-            },
-            items: [{
-                    type: "doc",
-                    docId: "intro",
-                    position: "left",
-                    label: "Tutorial",
-                },
-                // { to: "/blog", label: "Blog", position: "left" },
-                {
-                    href: "https://github.com/FlutterQueen/queen",
-                    label: "GitHub",
-                    position: "right",
-                },
-            ],
+    ({
+      navbar: {
+        title: "Queen 👑",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg",
         },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Tutorial",
+          },
+          // { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://github.com/FlutterQueen/queen",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
 
-        footer: {
-            style: "dark",
-            links: [{
-                    title: "Docs",
-                    items: [{
-                        label: "Tutorial",
-                        to: "/docs/intro",
-                    }, ],
-                },
-                {
-                    title: "Community",
-                    items: [{
-                            label: "Stack Overflow",
-                            href: "https://stackoverflow.com/questions/tagged/queen",
-                        },
-
-                        // {
-                        //     label: "Twitter",
-                        //     href: "https://twitter.com/docusaurus",
-                        // },
-                    ],
-                },
-                {
-                    title: "More",
-                    items: [
-                        // {
-                        //     label: "Blog",
-                        //     to: "/blog",
-                        // },
-                        {
-                            label: "GitHub",
-                            href: "https://github.com/FlutterQueen/queen",
-                        },
-                    ],
-                },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/intro",
+              },
             ],
-            copyright: `Copyright © ${new Date().getFullYear()} FlutterQueen .`,
-        },
-        prism: {
-            theme: lightCodeTheme,
-            darkTheme: darkCodeTheme,
-            additionalLanguages: ["dart"],
-        },
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/queen",
+              },
+
+              // {
+              //     label: "Twitter",
+              //     href: "https://twitter.com/docusaurus",
+              // },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              // {
+              //     label: "Blog",
+              //     to: "/blog",
+              // },
+              {
+                label: "GitHub",
+                href: "https://github.com/FlutterQueen/queen",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} FlutterQueen .`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ["dart"],
+      },
     }),
 };
 
--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -2,63 +2,59 @@ import React from "react";
 import clsx from "clsx";
 import styles from "./HomepageFeatures.module.css";
 // TODO :: update features images
-const FeatureList = [{
-        title: "Easy to Use",
-        Svg: require("../../static/img/undraw_docusaurus_mountain.svg").default,
-        description: ( <
-            >
-            Queen was designed to make app development simpler, more readable and easier to work with. { " " } <
-            />
-        ),
-    },
-    {
-        title: "Focus on What Matters",
-        Svg: require("../../static/img/undraw_docusaurus_tree.svg").default,
-        description: < > Queen Lets You Focus On Your More on Business logic < />,
-    },
-    {
-        title: "Powered by Flutter",
-        Svg: require("../../static/img/undraw_docusaurus_react.svg").default,
-        description: ( <
-            > { " " }
-            under the hood queen is just a collection of packages built to work together { " " } <
-            />
-        ),
-    },
+const FeatureList = [
+  {
+    title: "Easy to Use",
+    Svg: require("../../static/img/undraw_docusaurus_mountain.svg").default,
+    description: (
+      <>
+        Queen was designed to make app development simpler, more readable and
+        easier to work with.{" "}
+      </>
+    ),
+  },
+  {
+    title: "Focus on What Matters",
+    Svg: require("../../static/img/undraw_docusaurus_tree.svg").default,
+    description: <> Queen lets you focus on Your app more on business logic </>,
+  },
+  {
+    title: "Powered by Flutter",
+    Svg: require("../../static/img/undraw_docusaurus_react.svg").default,
+    description: (
+      <>
+        {" "}
+        under the hood queen is just a collection of packages built to work
+        together{" "}
+      </>
+    ),
+  },
 ];
 
 function Feature({ Svg, title, description }) {
-    return ( <
-        div className = { clsx("col col--4") } >
-        <
-        div className = "text--center" >
-        <
-        Svg className = { styles.featureSvg }
-        alt = { title }
-        />{" "} <
-        /div>{" "} <
-        div className = "text--center padding-horiz--md" >
-        <
-        h3 > { title } < /h3> <p> {description} </p > { " " } <
-        /div>{" "} <
-        /div>
-    );
+  return (
+    <div className={clsx("col col--4")}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} alt={title} />{" "}
+      </div>{" "}
+      <div className="text--center padding-horiz--md">
+        <h3> {title} </h3> <p> {description} </p>{" "}
+      </div>{" "}
+    </div>
+  );
 }
 
 export default function HomepageFeatures() {
-    return ( <
-        section className = { styles.features } >
-        <
-        div className = "container" >
-        <
-        div className = "row" > { " " } {
-            FeatureList.map((props, idx) => ( <
-                Feature key = { idx } {...props }
-                />
-            ))
-        } { " " } <
-        /div>{" "} <
-        /div>{" "} <
-        /section>
-    );
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {" "}
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}{" "}
+        </div>{" "}
+      </div>{" "}
+    </section>
+  );
 }
